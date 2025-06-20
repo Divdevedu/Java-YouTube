a0c1db4c2d79a5ab9fa561e8052d53189bdd7b07
Divdevedu <divdevedu@gmail.com>
2025-06-20 18:58:20 +0530

    Updated comments for clarity

---
+++ Basic.java
@@ -0,0 +1,8 @@
+public class Basic {
+    public static void main(String[] args) {
+
+        //args = arguments
+        //strings = collection or characters, text.
+        //method or function = machine
+    }
+}
