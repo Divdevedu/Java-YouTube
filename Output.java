09426079052aa7e1d106f5e6fe96d45977fdb0d1
Divdevedu <divdevedu@gmail.com>
2025-06-21 19:05:35 +0530

    Updated comments for clarity

---
+++ Output.java
@@ -0,0 +1,91 @@
+public class Output {
+    public static void main(String[] args) {
+
+
+        //numbers
+        //integers
+        //() = for input
+
+        System.out.println(20);
+
+        System.out.println(200);
+
+        System.out.println(12545512);
+
+        //decimal numbers, floating-point numbers
+
+        System.out.println(12.22);
+
+        //calculations
+        System.out.println(23+34);
+        System.out.println(23*34);
+        System.out.println(23-34);
+        System.out.println(23/34);
+
+        //BODMAS rule
+        System.out.println(23+65*88/2-99);
+
+        //text - words, sentences
+
+        System.out.println("Rohan");
+
+        //character
+        System.out.println("a");
+
+
+        //sentences
+        System.out.println("my name is rohan and i am 20 years old");
+
+        System.out.println("the sum of 23 and 34 is 23+34");
+
+        System.out.println("System.out.println()");
+
+        System.out.println("public class Output");
+
+        System.out.println("the sum of 23 and 34 is" + 23+34);
+        System.out.println("my age" + 23+3);
+
+        //addition
+        System.out.println(23+34);
+
+        //concatenation
+        System.out.println("Hello " + "world");
+
+        System.out.println("my " + "name " + "is " + "rohan");
+
+        System.out.println("hello" + " " + "world");
+
+        //println()
+
+        // ln = new line
+
+        System.out.println("hello"); // new line create
+        System.out.println("world");
+
+        //print()
+
+        System.out.print("hello" );  // no ln, no new line will create
+        System.out.print( " "+ "world");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
